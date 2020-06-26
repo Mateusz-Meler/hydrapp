d45c041e3ddd3e578271520e3ada883ab0675360
Mateusz Meler <mateuszmeler@icloud.com>
2020-06-26 15:53:09 +0200

    final js

--- src/js/index.js
+++ src/js/index.js
@@ -10,16 +10,30 @@ import "../scss/main.scss";
 const addGlass = document.querySelector(".button--js");
 const removeGlass = document.querySelector(".secondary-button--js");
 const counter = document.querySelector(".glass__number--js");
-
+const key = new Date().toLocaleString().slice(0, 10);
 let glassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+     glassCounter = localStorageValue;
+} else {
+localStorage.setItem(key, 0);
+}
+counter.innerHTML = glassCounter;
+
 
 addGlass.addEventListener('click', () => {
 glassCounter++;
 counter.innerHTML = glassCounter;
+localStorage.setItem(key, glassCounter);
 })
 
 removeGlass.addEventListener('click', () => {
+    if (glassCounter > 0){
     glassCounter--;
+    
+}
     counter.innerHTML = glassCounter;
-
+    localStorage.setItem(key, glassCounter);
     })
