9d90d0e7b70790c249fe24fcbdd24e7c9a240069
Mateusz Meler <mateuszmeler@icloud.com>
2020-06-26 14:36:50 +0200

    js counter

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,3 +6,20 @@ import '../scss/main.scss';
 
 /* place your code below */
 
+
+const addGlass = document.querySelector(".button--js");
+const removeGlass = document.querySelector(".secondary-button--js");
+const counter = document.querySelector(".glass__number--js");
+
+let glassCounter = 0;
+
+addGlass.addEventListener('click', () => {
+glassCounter++;
+counter.innerHTML = glassCounter;
+})
+
+removeGlass.addEventListener('click', () => {
+    glassCounter--;
+    counter.innerHTML = glassCounter;
+
+    })
